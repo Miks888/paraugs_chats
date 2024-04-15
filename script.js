63292419733008c5cc903fc163fcafe3729c544c
Miks888 <112856568+Miks888@users.noreply.github.com>
2024-04-15 15:41:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1);
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+    let zina = document.querySelector("zina");
+}   let zinas = document.querySelector("cataZina");
+    zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
